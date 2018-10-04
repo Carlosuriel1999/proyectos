00325f0c14de2f39c72503249d285c3df32dc803
carlos uriel <42219332+Carlosuriel1999@users.noreply.github.com>
2018-10-04 17:58:38 -0500

    Add files via upload

---
+++ ProyectoRopa/ProyectoRopa/ProyectoRopa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoRopa
+//
+//  Created by Laboratorio UNAM-Apple 16 on 04/10/18.
+//  Copyright © 2018 Agustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
